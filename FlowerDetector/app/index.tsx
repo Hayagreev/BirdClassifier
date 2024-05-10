727a681fa6154b389ef9470da1343615bda602b8
Hayagreev Dilip <hayagreev.dilip@gmail.com>
2024-05-10 16:00:37 -0700

    Added title to home page

--- FlowerDetector/app/index.tsx
+++ FlowerDetector/app/index.tsx
@@ -1,10 +1,13 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import BirdCard from "./components/BirdCard";
 import ImagePickerExample from "./components/Image_Picker";
 
 export default function Index() {
   return (
     <View>
+      <Text style={{fontWeight: 'bold', fontStyle: 'italic', fontSize: 40, textAlign: 'center', backgroundColor: 'lightblue'}}>
+        Bird Classifier
+      </Text>
       <ImagePickerExample />
     </View>
   );
