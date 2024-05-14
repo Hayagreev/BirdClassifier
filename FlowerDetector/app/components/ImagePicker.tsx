73d4b760af9f4f253839d47366875259b48b1e00
Hayagreev Dilip <hayagreev.dilip@gmail.com>
2024-05-13 20:05:35 -0700

    Implemented obtaining predictions

--- FlowerDetector/app/components/ImagePicker.tsx
+++ FlowerDetector/app/components/ImagePicker.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import { Button, Image, View, StyleSheet, Text } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as ImageManipulator from 'expo-image-manipulator';
+import * as FileSystem from 'expo-file-system';
+import * as tf from '@tensorflow/tfjs'
+import { decodeJpeg } from '@tensorflow/tfjs-react-native'
+
 
 export default function ImagePickerExample() {
   const [image, setImage] = useState("");
@@ -30,6 +34,30 @@ export default function ImagePickerExample() {
       [{ resize: { width: 180, height: 180 } }],
       { format: ImageManipulator.SaveFormat.JPEG }
     );
+
+
+    const imgB64 = await FileSystem.readAsStringAsync(manipResult.uri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    await tf.ready();
+
+    const imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
+    const raw = new Uint8Array(imgBuffer)
+    var imageTensor = decodeJpeg(raw);
+
+    await fetch("http://localhost:8501/v1/models/model_b:predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "instances": [imageTensor.arraySync()],
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(JSON.stringify(responseData));
+      })
  
     console.log('Image submitted: ', manipResult.uri);
   };
